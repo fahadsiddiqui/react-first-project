2751ed717cf488f6573c94d3c8595394286adc42
Fahad <fsdqui@gmail.com>
2019-01-09 16:27:15 +0500

    Handler.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Entity from './Entity/Entity';
 
 class App extends Component {
-  render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, this is my first page in React.js. I love it already!</h1>
-    //   </div>
-    // );
+  state = {
+    entities: [
+      { frequency: 26, nerType: "ORGANIZATION" },
+      { frequency: 22, nerType: "ORGANIZATION" },
+      { frequency: 21, nerType: "PERSON" },
+      { frequency: 23, nerType: "ORGANIZATION" },
+      { frequency: 20, nerType: "PERSON" }
+    ]
+  };
 
-    // this is alternative of the JS code written above.
-    return React.createElement('div', {className: 'App'}, React.createElement('h2', null, 'Does this even work?'));
+  changeStateHandler = () => {
+    this.setState({
+      entities: [
+        { frequency: 126, nerType: "ORGANIZATION" },
+        { frequency: 23, nerType: "PERSON" },
+        { frequency: 21, nerType: "PERSON" },
+        { frequency: 28, nerType: "PERSON" },
+        { frequency: 20, nerType: "PERSON" }
+      ]
+    });
+  };
+
+  changedHandler = (event) => {
+    this.setState({
+      entities: [
+        { frequency: 126, nerType: "ORGANIZATION" },
+        { frequency: 23, nerType: "PERSON" },
+        { frequency: event.target.value, nerType: "PERSON" },
+        { frequency: 28, nerType: "PERSON" },
+        { frequency: 20, nerType: "PERSON" }
+      ]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, this is my first page in React.js. I love it already!</h1>
+        <button onClick={this.changeStateHandler}>Change State</button>
+        <Entity frequency={this.state.entities[0].frequency} nerType={this.state.entities[0].nerType}>My thing!</Entity>
+        <Entity frequency={this.state.entities[1].frequency} nerType={this.state.entities[1].nerType}/>
+        <Entity clicked={this.changedHandler} frequency={this.state.entities[2].frequency} nerType={this.state.entities[2].nerType}/>
+        <Entity frequency={this.state.entities[3].frequency} nerType={this.state.entities[3].nerType}/>
+        <Entity frequency={this.state.entities[4].frequency} nerType={this.state.entities[4].nerType}/>
+      </div>
+    );
   }
 }
 
