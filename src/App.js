601d996e581056271a1976d670101a575c58c675
Fahad <fsdqui@gmail.com>
2019-01-16 16:23:59 +0500

    Fixed.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,97 @@
 import React, { Component } from 'react';
 import './App.css';
-import Entity from './Entity/Entity';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    entities: [
-      { frequency: 26, nerType: "ORGANIZATION" },
-      { frequency: 22, nerType: "ORGANIZATION" },
-      { frequency: 21, nerType: "PERSON" },
-      { frequency: 23, nerType: "ORGANIZATION" },
-      { frequency: 20, nerType: "PERSON" }
-    ]
-  };
+    persons: [
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-  changeStateHandler = () => {
-    this.setState({
-      entities: [
-        { frequency: 126, nerType: "ORGANIZATION" },
-        { frequency: 23, nerType: "PERSON" },
-        { frequency: 21, nerType: "PERSON" },
-        { frequency: 28, nerType: "PERSON" },
-        { frequency: 20, nerType: "PERSON" }
-      ]
-    });
-  };
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id === id;
+    } );
 
-  changedHandler = (event) => {
-    this.setState({
-      entities: [
-        { frequency: 126, nerType: "ORGANIZATION" },
-        { frequency: 23, nerType: "PERSON" },
-        { frequency: event.target.value, nerType: "PERSON" },
-        { frequency: 28, nerType: "PERSON" },
-        { frequency: 20, nerType: "PERSON" }
-      ]
-    });
-  };
+    const person = {
+      ...this.state.persons[personIndex]
+    };
 
-  render() {
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( { persons: persons } );
+  }
+
+  deletePersonHandler = ( personIndex ) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice( personIndex, 1 );
+    this.setState( { persons: persons } );
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.persons.map( ( person, index ) => {
+            return <Person
+              click={() => this.deletePersonHandler( index )}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={( event ) => this.nameChangedHandler( event, person.id )} />
+          } )}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if ( this.state.persons.length <= 2 ) {
+      classes.push( 'red' ); // classes = ['red']
+    }
+    if ( this.state.persons.length <= 1 ) {
+      classes.push( 'bold' ); // classes = ['red', 'bold']
+    }
+
     return (
       <div className="App">
-        <h1>Hi, this is my first page in React.js. I love it already!</h1>
-        <button onClick={this.changeStateHandler}>Change State</button>
-        <Entity frequency={this.state.entities[0].frequency} nerType={this.state.entities[0].nerType}>My thing!</Entity>
-        <Entity frequency={this.state.entities[1].frequency} nerType={this.state.entities[1].nerType}/>
-        <Entity clicked={this.changedHandler} frequency={this.state.entities[2].frequency} nerType={this.state.entities[2].nerType}/>
-        <Entity frequency={this.state.entities[3].frequency} nerType={this.state.entities[3].nerType}/>
-        <Entity frequency={this.state.entities[4].frequency} nerType={this.state.entities[4].nerType}/>
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join( ' ' )}>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
